b97c2ee49467dfb24a20c7b195591e81d20dc765
Yeowoon <woons.2016@gmail.com>
2016-01-26 16:14:26 +0900

    dplyr

---
+++ next_journalism_dplyr/next_journalism_dplyr.R
@@ -0,0 +1,31 @@
+#-------------------------------
+#install.packages("dplyr")     
+#install.packages("ggplot2")   
+#-------------------------------
+
+library(dplyr)
+library(ggplot2)
+
+#filter_function
+data <- diamonds
+data_result <- filter(data, cut=="Ideal")
+data_result <- filter(data, cut=="Good", color=="E")
+View(data_result)
+
+#select_function
+head(data)
+data_result <- select(data, carat, cut, color, price, clarity) %>% filter(price > 340)
+View(data_result)
+
+#Mutate_function
+data_resultl <- mutate(data_result, price_per_carat = price/carat)
+View(data_result)
+
+#Summurize_function
+summarize(data_result, avg_price = mean(price, na.rm = TRUE) )
+
+#Pipe_Operate
+data_result <- diamonds %>%
+  filter(cut=="Ideal") %>%
+  select(carat, cut, color, price, clarity) %>%
+  mutate(price_per_carat = price/carat)
